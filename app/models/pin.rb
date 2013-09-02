75579be4b30462479eb4ab6d1d9008a26d255d26
seth freedman <freedman.seth@gmail.com>
2013-09-01 22:24:07 -0400

    add Image Uploading via URL

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -11,7 +11,7 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	def image_remote_url=(url_value)
-		self.image = URI.parse(url.value) unless url_value.blank?
+		self.image = URI.parse(url_value) unless url_value.blank?
 		super
 	end
 end
